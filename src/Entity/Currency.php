4b56314b8066b77012eef8ec2f3e955574137177
Bartłomiej Mytnik <bartlomiej.mytnik@ec-at.com>
2023-03-09 14:56:46 +0100

    Currency entity created, database migration added

---
+++ src/Entity/Currency.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CurrencyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 64)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 16)]
+    private ?string $currencyCode = null;
+
+    #[ORM\Column]
+    private ?int $exchangeRate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currencyCode;
+    }
+
+    public function setCurrencyCode(string $currencyCode): self
+    {
+        $this->currencyCode = $currencyCode;
+
+        return $this;
+    }
+
+    public function getExchangeRate(): ?int
+    {
+        return $this->exchangeRate;
+    }
+
+    public function setExchangeRate(int $exchangeRate): self
+    {
+        $this->exchangeRate = $exchangeRate;
+
+        return $this;
+    }
+}
